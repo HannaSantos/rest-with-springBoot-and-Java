8ecdd333cb220bdc6774fb42b751308a0ad215be
HannaSantos <hanna.santos@bandtec.com.br>
2023-01-10 11:56:56 -0300

    refaturando o exception

--- api-getway-v2/src/main/java/br/com/apigetway/exceptions/ResourceNotFoundException.java
+++ api-getway-v2/src/main/java/br/com/apigetway/exceptions/ResourceNotFoundException.java
@@ -4,12 +4,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 // Essa classe sempre vai retorno um código de erro BAD REQUEST
-@ResponseStatus(HttpStatus.BAD_REQUEST)
-public class UnsupportedMathOperationException extends RuntimeException {
-
-    public UnsupportedMathOperationException(String ex) {
-        super(ex);
-    }
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class ResourceNotFoundException extends RuntimeException {
 
     private static final long serialVersionUID = 1L;
+
+    public ResourceNotFoundException(String ex) {
+        super(ex);
+    }
 }
--- api-getway-v2/src/main/java/br/com/apigetway/exceptions/handler/CustomizedResponseEntityExceptionHandler.java
+++ api-getway-v2/src/main/java/br/com/apigetway/exceptions/handler/CustomizedResponseEntityExceptionHandler.java
@@ -3,7 +3,7 @@ package br.com.apigetway.exceptions.handler;
 // Manipulador de Execeções
 
 import br.com.apigetway.exceptions.ExceptionResponse;
-import br.com.apigetway.exceptions.UnsupportedMathOperationException;
+import br.com.apigetway.exceptions.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import java.lang.reflect.Executable;
 import java.util.Date;
 
 @ControllerAdvice // é utilizada para lidar com exceções lançadas em qualquer lugar da sua aplicação, não só pelo controller.
@@ -31,8 +30,8 @@ public class CustomizedResponseEntityExceptionHandler  extends ResponseEntityExc
         return new ResponseEntity<>(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
-    @ExceptionHandler(UnsupportedMathOperationException.class)
-    public final ResponseEntity<ExceptionResponse> handleBadRequestExceptions(Exception ex, WebRequest request){
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public final ResponseEntity<ExceptionResponse> handleNotFoundExceptions(Exception ex, WebRequest request){
 
         ExceptionResponse exceptionResponse = new ExceptionResponse(
                 new Date(),
@@ -40,6 +39,6 @@ public class CustomizedResponseEntityExceptionHandler  extends ResponseEntityExc
                 request.getDescription(false)
         );
 
-        return new ResponseEntity<>(exceptionResponse, HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(exceptionResponse, HttpStatus.NOT_FOUND);
     }
 }
